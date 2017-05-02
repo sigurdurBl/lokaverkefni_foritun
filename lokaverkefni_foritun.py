822e4ad0d871297fa1e9058012ceecef094099ab
sigurdurBl <sigurdureg210@nemi.tskoli.is>
2017-05-02 15:22:07 +0000

    aðins að breita coðanum

--- lokaverkefni_foritun.py
+++ lokaverkefni_foritun.py
@@ -58,64 +58,19 @@ while len(notandi_stokur) != 0 and len(tolva_stokur) != 0:
     print("-----------------------------------------")
     print("einkun fyrir maltir-8:", tolva_stokur[0][8])
 
+    if notandi_stokur > tolva_stokur:
+        print("þú vanst  þingd",notandi_stokur[0][val],tolva_stokur[0][val])
+        notandi_stokur.append(tolva_stokur[0])
+        notandi_stokur.append(notandi_stokur[0])
+    elif tolva_stokur > notandi_stokur:
+        print("talvan vann  þingd",notandi_stokur[0][val],tolva_stokur[0][val])
+        tolva_stokur.append(tolva_stokur[0])
+        tolva_stokur.append(notandi_stokur[0])
+    elif notandi_stokur == tolva_stokur:
+        print("það var jafntefli  þingd",notandi_stokur[0][val],tolva_stokur[0][val])
+    tolva_stokur.remove(tolva_stokur[0])
+    notandi_stokur.remove([0])
 
-
-    if val == 1:
-        if notandi_stokur > tolva_stokur:
-            print("þú vanst  þingd",notandi_stokur[0][1],tolva_stokur[0][1])
-        elif tolva_stokur > notandi_stokur:
-            print("talvan vann  þingd",notandi_stokur[0][1],tolva_stokur[0][1])
-        elif notandi_stokur == tolva_stokur:
-            print("það var jafntefli  þingd",notandi_stokur[0][1],tolva_stokur[0][1])
-    if val ==2:
-        if notandi_stokur > tolva_stokur:
-            print("þú vanst  mjólkurlangi",notandi_stokur[0][2],tolva_stokur[0][2])
-        elif tolva_stokur > notandi_stokur:
-            print("talvan vann  mjólkurlangi",notandi_stokur[0][2],tolva_stokur[0][2])
-        elif notandi_stokur == tolva_stokur:
-            print("það var jafntefli  mjólkurlangi",notandi_stokur[0][2],tolva_stokur[0][2])
-    if val ==3:
-        if notandi_stokur > tolva_stokur:
-            print("þú vanst  einkun ullar",notandi_stokur[0][3],tolva_stokur[0][3])
-        elif tolva_stokur > notandi_stokur:
-            print("talvan vann  einkun ullar",notandi_stokur[0][3],tolva_stokur[0][3])
-        elif notandi_stokur == tolva_stokur:
-            print("það var jafntefli  einkun ullar",notandi_stokur[0][3],tolva_stokur[0][3])
-    if val ==4:
-        if notandi_stokur > tolva_stokur:
-            print("þú vanst  einkun",notandi_stokur[0][4],tolva_stokur[0][4])
-        elif tolva_stokur > notandi_stokur:
-            print("talvan vann  einkun",notandi_stokur[0][4],tolva_stokur[0][4])
-        elif notandi_stokur == tolva_stokur:
-            print("það var jafntefli  einkun",notandi_stokur[0][4],tolva_stokur[0][4])
-    if val ==5:
-        if notandi_stokur > tolva_stokur:
-            print("þú vanst  einkun læris",notandi_stokur[0][5],tolva_stokur[0][5])
-        elif tolva_stokur > notandi_stokur:
-            print("talvan vann",notandi_stokur[0][5],tolva_stokur[0][5])
-        elif notandi_stokur == tolva_stokur:
-            print("það var jafntefli",notandi_stokur[0][5],tolva_stokur[0][5])
-    if val ==6:
-        if notandi_stokur > tolva_stokur:
-            print("þú vanst",notandi_stokur[0][6],tolva_stokur[0][6])
-        elif tolva_stokur > notandi_stokur:
-            print("talvan vann",notandi_stokur[0][6],tolva_stokur[0][6])
-        elif notandi_stokur == tolva_stokur:
-            print("það var jafntefli",notandi_stokur[0][6],tolva_stokur[0][6])
-    if val ==7:
-        if notandi_stokur > tolva_stokur:
-            print("þú vanst",notandi_stokur[0][7],tolva_stokur[0][7])
-        elif tolva_stokur > notandi_stokur:
-            print("talvan vann",notandi_stokur[0][7],tolva_stokur[0][7])
-        elif notandi_stokur == tolva_stokur:
-            print("það var jafntefli",notandi_stokur[0][7],tolva_stokur[0][7])
-    if val ==8:
-        if notandi_stokur > tolva_stokur:
-            print("þú vanst",notandi_stokur[0][8],tolva_stokur[0][8])
-        elif tolva_stokur > notandi_stokur:
-            print("talvan vann",notandi_stokur[0][8],tolva_stokur[0][8])
-        elif notandi_stokur == tolva_stokur:
-            print("það var jafntefli",notandi_stokur[0][8],tolva_stokur[0][8])
 
 
 
